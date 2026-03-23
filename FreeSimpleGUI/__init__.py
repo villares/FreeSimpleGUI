1793d011997937a803e0f13aab1cf3207c0b5e8d
Orsiris de Jong <ozy@netpower.fr>
2026-03-23 14:41:28 +0100

    Improve DPI awareness check for Windows Server
    Updated DPI awareness handling for Windows Server versions.

--- FreeSimpleGUI/__init__.py
+++ FreeSimpleGUI/__init__.py
@@ -8572,13 +8572,19 @@ def set_options(
     if dpi_awareness is True:
         if running_windows():
             try:
-                if platform.release() == '7':
+                # Windows Server might return a string like "2022Server" on Python 3.13
+                # whereas it would return 10 on Python 3.7
+                # If we happen to find "server" in our release, let's just assume we're running
+                # a recent windows server version
+                if "Server" in platform.release():
+                    ctypes.windll.shcore.SetProcessDpiAwareness(1)
+                elif platform.release() == '7':
                     ctypes.windll.user32.SetProcessDPIAware()
                 # Windows 8, 10 and 11 use the following API
                 elif int(platform.release()) >= 8:
                     ctypes.windll.shcore.SetProcessDpiAwareness(1)
-            except TypeError:
-                # If platform.release isn't a number, just ignore the API
+            except (ValueError, TypeError):
+                # If platform.release isn't a number or cant be computed here, just igonore it
                 pass
 
     if scaling is not None:
